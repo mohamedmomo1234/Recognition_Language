325c5f200173a8c88cfc5e4c2b87ced759efa568
Mohamed-said-abdalaziz12 <mohamedshahin2050softwere@gmail.com>
2026-09-17 20:02:15 +0300

    Update realtime.py

--- realtime.py
+++ realtime.py
@@ -3,11 +3,11 @@ import mediapipe as mp
 import numpy as np
 import joblib
 
-# تحميل الموديل المدرب مسبقاً
+# Load the pre-trained model
 model = joblib.load("best_sign_model.pkl")
 print("[INFO] Model loaded successfully for Real-Time Prediction.")
 
-# تهيئة MediaPipe Hands
+# initial MediaPipe Hands
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     static_image_mode=False,
@@ -17,7 +17,7 @@ hands = mp_hands.Hands(
 )
 mp_drawing = mp.solutions.drawing_utils
 
-# فتح الويب كام
+# Open webcam
 cap = cv2.VideoCapture(0)
 
 print("[INFO] Starting Webcam... Press 'q' to exit.")
@@ -28,7 +28,9 @@ while cap.isOpened():
         print("Ignoring empty camera frame.")
         continue
 
-    # انعكاس الصورة لتبدو كمرآة، وتحويل الألوان لـ RGB
+    
+    # Mirroring the image and converting colors to RGB
+    
     frame = cv2.flip(frame, 1)
     h, w, c = frame.shape
     image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -39,17 +41,17 @@ while cap.isOpened():
     
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
-            # رسم نقاط اليد على الشاشة
+            # Drawing hand points on the screen
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
             
-            # استخراج إحداثيات x و y فقط (42 قيمة)
+            # extract coordinates x و y  (42 values)
             landmarks = []
             for lm in hand_landmarks.landmark:
                 landmarks.append([lm.x, lm.y])
             
             landmarks = np.array(landmarks, dtype=np.float32)
             
-            # نفس خطوات الـ Normalization تماماً كما في ملف التدريب
+            #  Normalization   
             wrist = landmarks[0]
             landmarks = landmarks - wrist
             
@@ -59,11 +61,11 @@ while cap.isOpened():
                 
             flattened_features = landmarks.flatten().reshape(1, -1)
             
-            # التوقع باستخدام الموديل المدرب
+            #Prediction using the trained model
             prediction = model.predict(flattened_features)
             predicted_label = str(prediction[0])
 
-    # عرض النتيجة على الشاشة
+    # Display the result on the screen.
     cv2.putText(
         frame, 
         f"Prediction: {predicted_label}", 
